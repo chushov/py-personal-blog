d1c0c0781016ed4d11e113f8131d64c304278503
dmitrychushov <work@chushov.com>
2021-07-06 20:59:09 +0300

    del garbage

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -16,16 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from django.conf import settings
-from django.conf.urls.static import static
-from main.views import HomeView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
     path("blog/", include("blog.urls")),
-    path("", HomeView.as_view(), name="home-index"),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
