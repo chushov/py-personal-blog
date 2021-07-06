2242202a607bfedac40d7c8e0aa00154fa3196d9
dmitrychushov <work@chushov.com>
2021-07-06 20:36:23 +0300

    add urls, add indexpage

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from main.views import HomeView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
     path("blog/", include("blog.urls")),
-    path('', TemplateView.as_view(template_name='personal_portfolio/main.html')),
+    path('', HomeView.as_view(), name='home-index'),
 ]
+if settings.DEBUG:
+               urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
