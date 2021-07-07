ccec676bd0d245e10f830cd5ad2599ccaae6855f
asyncee <asyncee@users.noreply.github.com>
2021-07-07 10:21:15 +0300

    Enable homepage

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import include
+from django.urls import path
 
+from main.views import Homepage
 
 urlpatterns = [
+    # Homepage path definition.
+    path("", Homepage.as_view()),
+
+    # Same as above but using regular expressions (just an example).
+    # from django.urls import re_path
+    # re_path(r"^$", Homepage.as_view()),
+
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
     path("blog/", include("blog.urls")),
