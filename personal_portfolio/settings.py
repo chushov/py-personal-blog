56dc92d5e70633f39eb908135fab15a2503f3f9f
dmitrychushov <work@chushov.com>
2021-07-07 20:51:14 +0300

    add custom styles

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -115,6 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
