d6a8b2c8f22615268160337d10e279ef76bb140a
chushov <work@chushov.com>
2021-08-05 15:22:20 +0300

    Добавлен вывод тайтлов проектов из модели Проектов

--- main/views.py
+++ main/views.py
@@ -1,5 +1,10 @@
 from django.views.generic import TemplateView
-
+from projects.models import Project
 
 class Homepage(TemplateView):
     template_name = "main/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['projects'] = Project.objects.all()
+        return context
