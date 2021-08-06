d183821d788ca0b1cbd7373c049fe68c5edf6877
chushov <work@chushov.com>
2021-08-06 15:41:18 +0300

    Переименовал модели в админке, добавил verbose_name, разобрался с выводом заголовка для постов в админке

--- blog/models.py
+++ blog/models.py
@@ -2,19 +2,40 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, verbose_name="Название")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
-    categories = models.ManyToManyField("Category", related_name="posts")
+    created_on = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
+    last_modified = models.DateTimeField(auto_now=True, verbose_name="Последнее изменение")
+    categories = models.ManyToManyField("Category", related_name="posts", verbose_name="Категория")
+
+    class Meta:
+        verbose_name = "Запись"
+        verbose_name_plural = "Записи"
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
-    author = models.CharField(max_length=60)
+    author = models.CharField(max_length=60, verbose_name="Автор")
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True, verbose_name="Создан")
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, verbose_name="Пост")
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+
+    def __str__(self):
+        return self.post
--- projects/models.py
+++ projects/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=76)
-    description = models.TextField(max_length=150)
-    technology = models.CharField(max_length=20)
-    image = models.ImageField(upload_to="static/img/", blank=True, null=True)
+    title = models.CharField(max_length=76, verbose_name="Заголовок")
+    description = models.TextField(max_length=150, verbose_name="Описание")
+    technology = models.CharField(max_length=20, verbose_name="Технология")
+    image = models.ImageField(upload_to="static/img/", blank=True, null=True, verbose_name="Картинка")
+
+    class Meta:
+        verbose_name = "Проект"
+        verbose_name_plural = "Проекты"
+
+    def __str__(self):
+        return self.title
