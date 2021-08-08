2f0ef32a973752575f6be2df8cf490ca96db2899
chushov <work@chushov.com>
2021-08-08 16:45:22 +0300

    fix

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -3,12 +3,10 @@ from django.urls import include
 from django.urls import path
 from main.views import Homepage
 
-# from django.conf import settings
-# from django.conf.urls.static import static
 
 urlpatterns = [
     path("", Homepage.as_view()),
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
     path("blog/", include("blog.urls")),
-] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
